befdbef481f6c9060a5a7226a9e8b4573e8e49e2
dudwn2329 <dudwn2329@naver.com>
2019-05-06 16:36:06 +0900

    로그아웃까지 완료

---
+++ register_ok.php
@@ -0,0 +1,18 @@
+<?php
+$id = $_POST['id'];
+$name = $_POST['name'];
+$pw = $_POST['password'];
+
+
+require ("db.php");
+
+$sql = "insert into users (id, name, password) values (?, ?, PASSWORD(?))";
+$cnt = query($con, $sql, [$id, $name, $pw]);
+
+if($cnt == 0){
+    echo "회원가입 실패";
+}else{
+    msgAndGO("회원가입이 성공했습니다.","/index.php");
+    
+        
+}
